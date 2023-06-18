2b273867542085ac65e3cf10425d29ec5fd8b76d
Nazmul Alom <nazmulshuvo03@gmail.com>
2023-06-18 19:39:59 +0600

    hello solana updated

--- hello-solana/src/lib.rs
+++ hello-solana/src/lib.rs
@@ -2,7 +2,6 @@ use solana_program::{
     account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
 };
 
-// declare and export the program's entrypoint
 entrypoint!(process_instruction);
 
 pub fn process_instruction(
@@ -10,11 +9,8 @@ pub fn process_instruction(
     _accounts: &[AccountInfo],
     _instruction_data: &[u8],
 ) -> ProgramResult {
-    // log a message to the blockchain
-    msg!("Hello Solana!!");
-
-    // gracefully exit the program
+    msg!("Hello local Solana network!!");
     Ok(())
 }
 
-// ProgramId: 8ShnprrUeMPq7Rd2j3ofrwMiN78Qbv1vDX6YX7BHBzkW
+// Program Id: 9SHSMd79fEBfHq6WuavXb5PetfrkCJHPxwtVQ8BydWG9
